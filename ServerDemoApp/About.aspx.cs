c9cbcebf2b39b034b6fc87423c0784b6488d5d47
Nguyễn Thanh Tùng <tungnt185@live.com>
2015-11-25 03:22:21 +0700

    Add

---
+++ ServerDemoApp/About.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerDemoApp
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Test.AuthenticationServiceClient client = new Test.AuthenticationServiceClient();
+            bool result = client.Login("nttung", "123456", null, true);
+            Response.Write(result);
+        }
+    }
+}
